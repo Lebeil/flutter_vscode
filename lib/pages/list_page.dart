10eaab71294f76070eb57a09312c2e5e412916dd
Lebeil <liedel75@gmail.com>
2021-10-01 12:05:48 +0200

    add Page Login

---
+++ lib/pages/list_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'dart:developer';
+
+class ListPage extends StatefulWidget {
+  const ListPage({Key? key}) : super(key: key);
+
+  @override
+  _ListPageState createState() => _ListPageState();
+}
+
+class _ListPageState extends State<ListPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Text("Page liste des cours ...");
+  }
+}
---
+++ lib/pages/login_page.dart
@@ -0,0 +1,92 @@
+import 'package:flutter/material.dart';
+import 'dart:developer';
+
+import 'list_page.dart';
+
+class LoginPage extends StatelessWidget {
+  LoginPage({Key? key}) : super(key: key);
+
+  String mail = "";
+  String pass = "";
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text("Page de login"),
+          centerTitle: true,
+        ),
+        body: Column(
+          children: <Widget>[
+            _buildHeader(),
+            const SizedBox(height: 15),
+            _buildTitle(),
+            _buildForm(context),
+          ],
+        ));
+  }
+
+  // Fonction create header page login
+  Widget _buildHeader() {
+    return Image.network("https://formation-facile.fr/banner-cours-udemy.png");
+  }
+
+  // create title
+  Widget _buildTitle() {
+    return const Text(
+      'Connectez-vous',
+      style: TextStyle(fontSize: 28),
+    );
+  }
+
+  // create form
+  Widget _buildForm(BuildContext context) {
+    return Container(
+        padding: const EdgeInsets.all(15),
+        child: Column(
+          children: <Widget>[
+            TextFormField(
+              // récupérer le mail entré par l'utilisateur
+              onChanged: (newText) {
+                mail = newText;
+              },
+              decoration: const InputDecoration(labelText: "Votre mail ..."),
+            ),
+            const SizedBox(height: 10),
+            TextFormField(
+              decoration:
+                  const InputDecoration(labelText: "Votre mot de passe ..."),
+              onChanged: (newText) {
+                pass = newText;
+              },
+              obscureText: true,
+            ),
+            const SizedBox(height: 15),
+            ElevatedButton(
+                onPressed: () {
+                  // ignore: todo
+                  // TODO: Fonction de connexion
+                  if (mail == "123" && pass == "123") {
+                    //TODO: Vérifié via BDD
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => const ListPage()));
+                  } else {
+                    log("Erreur identifiant");
+                  }
+                },
+                child: const Text("Login"),
+                style: ElevatedButton.styleFrom(
+                  primary: Colors.red,
+                )),
+            TextButton(
+                onPressed: () {},
+                child: const Text("Mot de passe oublié ?"),
+                style: TextButton.styleFrom(
+                  primary: Colors.black,
+                ))
+          ],
+        ));
+  }
+}
